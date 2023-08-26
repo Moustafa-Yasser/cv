24c5efa8fd0876539eacba9f05730659f647023e
Mostafa <moustafa0yasser123@gmail.com>
2023-08-27 00:32:18 +0300

    chang somes colors & fix one proplem in js & add arwwo top scroll

--- js/main.js
+++ js/main.js
@@ -14,76 +14,15 @@ all.addEventListener("click", () => {
   list.style = "  transform: translateX(100%);";
 });
 // end menu
-// start valid form
-let firstName = document.getElementById("first");
-let lastName = document.getElementById("last");
-let emailValid = document.getElementById("email");
-let order = document.getElementById("help");
-let submit = document.getElementById("btn");
-let reg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-
-firstName.onblur = () => {
-  if (firstName.value === "") {
-    firstName.style.borderColor = "red";
-  } else {
-    firstName.style.borderColor = "green";
-  }
+// Start arrow top page
+let arrow = document.querySelector("body span");
+window.onscroll = () => {
+  this.scrollY >= 850
+    ? arrow.classList.add("block")
+    : arrow.classList.remove("block");
 };
-
-lastName.onblur = () => {
-  if (lastName.value === "") {
-    lastName.style.borderColor = "red";
-  } else {
-    lastName.style.borderColor = "green";
-  }
-};
-
-order.onblur = () => {
-  if (order.value === "") {
-    order.style.borderColor = "red";
-  } else {
-    order.style.borderColor = "green";
-  }
-};
-
-emailValid.onchange = function () {
-  if (emailValid.value.match(reg)) {
-    emailValid.style.borderColor = "green";
-  } else {
-    emailValid.style.borderColor = "red";
-  }
-};
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-});
-function Send() {
-  let name = `${document.getElementById("first").value} ${
-    document.getElementById("last").value
-  }`;
-  let email = document.getElementById("email").value;
-  let mess = document.getElementById("help").value;
-  let body = `Name: ${name} <br/> Email: ${email} <br/> Message: ${mess}`;
-  Email.send({
-    SecureToken: "51eff55e-d31b-45cb-93ce-e9b9a112ef6b",
-    To: "almtabynrsayl@gmail.com",
-    From: "almtabynrsayl@gmail.com",
-    Subject: "Questions Followers",
-    Body: body,
-  }).then((message) => {
-    if (
-      message == "OK" &&
-      emailValid.value.match(reg) &&
-      firstName.value !== "" &&
-      lastName.value !== "" &&
-      order.value !== ""
-    ) {
-      swal("عملية ناجحة", "تم إرسال رسالتك بنجاح", "success");
-    } else {
-      swal(
-        "عملية فاشلة",
-        "حدث خطأ ما، يرجي ملئ جميع الحقول بشكل صحيح",
-        "error"
-      );
-    }
+arrow.onclick = () => {
+  window.scrollTo({
+    top: 0,
   });
-}
+};
